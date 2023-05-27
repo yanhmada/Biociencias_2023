7e09ea4e2156641c63055895340ee763cffa1ce6
Yanet HM <98841526+yanhmada@users.noreply.github.com>
2023-05-27 09:14:07 -0700

    cambios tratamientos

--- stats_II/main.py
+++ stats_II/main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-#interactive = st.beta.container()
 
 import plotly.io as pio
 pio.renderers.default='notebook'
+from scipy import stats
+from scipy.stats.mstats import kruskal
 
 
 
@@ -13,10 +14,9 @@ dataset = st.container ()
 features = st.container ()
 
 with header:
-    st.title( 'Welcome to Biociencias 2023\n\n' 
+    st.title( 'Biociencias 2023\n\n' 
              'Maestria en Ciencia de Datos. Universidad de Sonora')
 
-st.header('Biociencias 2023')
 st.header(':blue[Observar diferencias en colonización ] _micorrícica_ :blue[entre plantas] ')
 
 text = ("Uno de los beneficios de la colonizacion micorrícica es el aumento de nutrientes, "
@@ -25,21 +25,21 @@ text = ("Uno de los beneficios de la colonizacion micorrícica es el aumento de 
         "* The Role of the Mycorrhizal Symbiosis in Nutrient Uptake of Plants and the Regulatory "
         "Mechanisms Underlying These Transport Processes")
 
-streamlined_text = text.replace("\n", " ").strip()
+st.write(text)
 
-print(streamlined_text)
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(("Base_datos_1.csv"))
+
+df_ = get_data()
 
 
-
-df_  = pd.read_csv(("Base_datos_1.csv"))
-
-#dict to update  species to common name 
+#Add common name
 dict_common  = {'V. farnesiana':'Vinorama','R. communis':'Ricino'}
 
-#update each
-for key, value in dict_common.items():
-    df_['Especie'] = df_['Especie'].replace(key, value)
+# agregar 'common_name' 
+df_['common_name'] = df_['Especie'].replace(dict_common)
 
+df_ = df_[(df_['Trat'] != 'J') & (df_['Trat'] != 'JI')] #remove Jal an
 
 #list for selection
 listd = ['P','As', 'Ca', 'Cu', 'Fe',
@@ -52,25 +52,46 @@ selected_columns = st.selectbox('Select Element to chart', options = listd, inde
 filtered_df = df_[selected_columns]
 
 
-# Crear la grafica de violin
-fig = px.violin(df_, x="Tejido", y="P", color="Especie", box=True, points="all", facet_col="Trat")
-
-# Agregar el tratamiento 
-fig = fig.update_layout(violingap=0, violingroupgap=0, violinmode="overlay")
+# Crear la grafica de caja
+fig = px.box(df_, x="common_name", y=selected_columns, color="common_name", points="all", facet_col="Trat")
 
 # Mostrar grafica
 st.write(fig)
 
+#Part 2
+# valores de 'Tratamiento' 
+trat_values = ['C', 'CI', 'J', 'JI', 'SN']
 
-# Crear la gráfica de histograma
-fig2 = px.histogram(df_, x=selected_columns, y="Especie", color="Tejido", facet_row="Trat")
+# iniciar diccionario
 
-fig2.update_layout(
-    barmode="overlay",  # Para superponer los histogramas
-    bargap=0.1,  # Espacio entre las barras
-    bargroupgap=0.05,  
-)
+dfs_vinorama = {}
 
-fig2.update_traces(opacity=0.55)
-# Mostrar la gráfica
-st.write(fig2)
+# Crear dataframe Vinorama
+for trat in trat_values:
+    # Filter the dataframe and select only the 'P' column
+    dfs_vinorama[trat] = df_[(df_['Trat'] == trat) & (df_['common_name'] == 'Vinorama')][[selected_columns]].copy()
+
+st.markdown("### Levene Test result")
+from scipy.stats import levene
+#data = dfs_vinorama.values.ravel()
+stat, p_value = levene(dfs_vinorama['C'][selected_columns], 
+                 dfs_vinorama['CI'][selected_columns], 
+                 dfs_vinorama['SN'][selected_columns])
+
+st.write("Segun la prueba  Levene el p-valor es :", p_value)
+
+st.markdown("###  Kruskal~ Wallis")
+from scipy.stats import levene
+#data = dfs_vinorama.values.ravel()
+stat, p_value = levene(dfs_vinorama['C'][selected_columns], 
+                 dfs_vinorama['CI'][selected_columns], 
+                 dfs_vinorama['SN'][selected_columns])
+
+
+#perform Kruskal-Wallis Test 
+statistic, pi_value = stats.kruskal(dfs_vinorama['C'][selected_columns], 
+                 dfs_vinorama['CI'][selected_columns], 
+                 dfs_vinorama['SN'][selected_columns])
+
+
+st.write("El p- valor para la prueba Kruskal - wallis es ", pi_value)
